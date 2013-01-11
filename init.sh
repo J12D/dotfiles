5ed62352d94a8b60275d9f99590a807308a9fd69
J12D <heinzderalligator@googlemail.com>
2013-01-11 14:13:56 +0100

    zshrc added (Thanks @adi64!)

--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 
 git submodule init && git submodule update
