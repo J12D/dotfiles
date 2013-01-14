178076d951071a1bb7a2c0a44b43d718547addf8
J12D <heinzderalligator@googlemail.com>
2013-01-14 14:37:30 +0100

    Sniff http GET & POST

--- fish/config.fish
+++ fish/config.fish
@@ -28,6 +28,10 @@ function myip
 	dig +short myip.opendns.com @resolver1.opendns.com
 end
 
+function sniff
+    sudo ngrep -d 'en1' -t '^(GET|POST)' 'tcp and port 80'
+end
+
 function calc
     echo $argv | bc
 end
@@ -78,3 +82,42 @@ set LSCOLORS = cxfxcxdxbxegedabagacad
 set PS1 "$PS1"'$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#D" | tr -d %) "$PWD")'
 
 set fish_greeting ""
+
+function fish_prompt --description 'Write out the prompt'
+    # Just calculate these once, to save a few cycles when displaying the prompt
+    if not set -q __fish_prompt_hostname
+        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+    end
+
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
+
+    if not set -q __git_cb
+        set __git_cb ":"(set_color brown)(git branch ^/dev/null | grep \* | sed 's/* //')(set_color normal)""
+    end
+
+    switch $USER
+
+        case root
+
+        if not set -q __fish_prompt_cwd
+            if set -q fish_color_cwd_root
+                set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+            else
+                set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+            end
+        end
+
+        printf '%s@%s:%s%s%s%s# ' $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" $__git_cb
+
+        case '*'
+
+        if not set -q __fish_prompt_cwd
+            set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+        end
+
+        printf '%s@%s:%s%s%s%s> ' $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" $__git_cb
+
+    end
+end
