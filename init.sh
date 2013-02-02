62a3e83d38b7156eae27cd5495644a321963cafb
J12D <heinzderalligator@googlemail.com>
2013-02-02 21:00:06 +0100

    Added tmux config

--- init.sh
+++ init.sh
@@ -6,5 +6,7 @@ ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/tmux/tmux.conf .tmux.conf
+ln -s ~/dotfiles/tmux/tmux .tmux
 
 git submodule init && git submodule update
