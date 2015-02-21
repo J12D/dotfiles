7d1a27c18e9995628732c03414399da0555a38d6
J12D <heinzderalligator@googlemail.com>
2015-02-21 22:38:12 +0100

    Fix tmux config in init.sh

--- init.sh
+++ init.sh
@@ -6,8 +6,8 @@ ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
-ln -s ~/dotfiles/tmux/tmux.conf .tmux.conf
-ln -s ~/dotfiles/tmux/tmux .tmux
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/tmux/tmux ~/.tmux
 
 git submodule init && git submodule update
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
