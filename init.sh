be5228f837e60177db74fba4f416e53f024c4a90
J12D <heinzderalligator@googlemail.com>
2016-07-31 16:29:08 +0100

    Use Carl's functions for installing dotfiles

--- init.sh
+++ init.sh
@@ -1,22 +1,68 @@
 #!/bin/bash
 
-# Symlink the files/folders to appropriate locations 
-ln -s ~/dotfiles/vim              ~/.vim
-ln -s ~/dotfiles/vim/vimrc        ~/.vimrc
-ln -s ~/dotfiles/vim/gvimrc       ~/.gvimrc
-ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
-ln -s ~/dotfiles/fish/iterm2_shell_integration.fish ~/.config/fish/iterm2_shell_integration.fish
-ln -s ~/dotfiles/git/gitconfig    ~/.gitconfig
-ln -s ~/dotfiles/zsh/zshrc        ~/.zshrc
-ln -s ~/dotfiles/tmux/tmux.conf   ~/.tmux.conf
-ln -s ~/dotfiles/tmux/tmux        ~/.tmux
+ROOT=$(cd "$(dirname "$0")" && pwd)
 
-git submodule init && git submodule update
+function green { printf "\033[32m$1\033[0m\n"; }
+function yellow { printf "\033[33m$1\033[0m\n"; }
+function red { printf "\033[31m$1\033[0m\n"; }
+
+function install {
+  local from="$1" to="$2" from_="$ROOT/$1" to_="$HOME/$2"
+
+  if [ ! -e "$from_" ]; then
+  red "ERROR: $from doesn't exist! This is an error in $0"
+  return 1
+  fi
+
+  if [ ! -e "$to_" ] ; then
+    if [ -L "$to_" ]; then
+      red "~/$to exists but doesn't point to a valid file, removing ..."
+      unlink $to_
+    fi
+    yellow "Linking ~/$to => $from"
+
+    if [ -d "$from_" ]; then
+      ln -s "$from_/" "$to_"
+    else
+      ln -s "$from_" "$to_"
+    fi
+  else
+    local link=$(readlink "$to_")
+    if [ "$?" == 0 -a \( "$link" == "$from_" -o "$link" == "$from_/" \) ]; then
+      green "Link ~/$to => $from already exists!"
+    else
+      red "Error linking ~/$to to $from: $to already exists!"
+    fi
+  fi
+}
+
+function install_dot {
+  install "$1" ".$1"
+}
+
+
+cd "$ROOT"
+git submodule init
+git submodule update
+
+
+# Symlink files/folders to appropriate locations 
+install vim                                .vim
+install vim/vimrc                          .vimrc
+install vim/gvimrc                         .gvimrc
+install fish/config.fish                   .config/fish/config.fish
+install fish/iterm2_shell_integration.fish .config/fish/iterm2_shell_integration.fish
+install git/gitconfig                      .gitconfig
+install zsh/zshrc                          .zshrc
+install tmux/tmux.conf                     .tmux.conf
+install tmux/tmux                          .tmux
+
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 else
-  echo "Vundle already initialized"
+  green "Vundle already initialized"
 fi
+green "Updating vim bundles"
 vim +BundleInstall +qall
-open ~/dotfiles/vim/Monaco_Linux-Powerline.ttf
-echo "Don't forget to install the fonts in $(pwd)/font"
+#open ~/dotfiles/vim/Monaco_Linux-Powerline.ttf
+green "Don't forget to install the fonts in $ROOT/font"
